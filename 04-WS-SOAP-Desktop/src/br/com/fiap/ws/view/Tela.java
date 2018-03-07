e28bba909b3a12f71a285919d79ae97ff96e6ed8
Leandro Romano <rm75244@fiap.com.br>
2018-03-07 20:55:59 -0300

    Updates

--- 04-WS-SOAP-Desktop/src/br/com/fiap/ws/view/Tela.java
+++ 04-WS-SOAP-Desktop/src/br/com/fiap/ws/view/Tela.java
@@ -2,10 +2,19 @@ package br.com.fiap.ws.view;
 
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Text;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
 
 public class Tela {
 
 	protected Shell shell;
+	private Text txtAm;
+	private Text txtNac;
+	private Text txtPs;
 
 	/**
 	 * Launch the application.
@@ -40,9 +49,39 @@ public class Tela {
 	 */
 	protected void createContents() {
 		shell = new Shell();
-		shell.setSize(450, 300);
+		shell.setSize(222, 202);
 		shell.setText("SWT Application");
+		
+		txtAm = new Text(shell, SWT.BORDER);
+		txtAm.setBounds(98, 10, 76, 21);
+		
+		txtNac = new Text(shell, SWT.BORDER);
+		txtNac.setBounds(98, 37, 76, 21);
+		
+		Label lblAm = new Label(shell, SWT.NONE);
+		lblAm.setBounds(10, 13, 55, 15);
+		lblAm.setText("AM:");
+		
+		Label lblNac = new Label(shell, SWT.NONE);
+		lblNac.setBounds(10, 40, 55, 15);
+		lblNac.setText("NAC:");
+		
+		Button btnCalcular = new Button(shell, SWT.NONE);
+		btnCalcular.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				
+			}
+		});
+		btnCalcular.setBounds(98, 90, 75, 25);
+		btnCalcular.setText("Calcular");
+		
+		Label lblPs = new Label(shell, SWT.NONE);
+		lblPs.setBounds(10, 125, 55, 15);
+		lblPs.setText("PS:");
+		
+		txtPs = new Text(shell, SWT.BORDER);
+		txtPs.setBounds(98, 121, 76, 21);
 
 	}
-
 }
